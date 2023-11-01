c03233db51501f437fe69233d9d1f319ab30dc0c
hamzut <hamzut2010@mail.ru>
2023-11-01 09:15:05 +0300

    sdf

--- main.js
+++ main.js
@@ -19,3 +19,18 @@ iPhone.addEventListener("click", function (event){
         activeButton.classList.remove("active");
     }
 })
+samsung.addEventListener("click", function (event){
+    let clicktarget = event.target;
+    let activeButton = samsung.querySelector(".active");
+    let priceItem = samsung.querySelector(".price");
+    let currentPrice;
+    let activeButtoncolor = samsung.querySelector(".active-border");
+    if (clicktarget.classList.contains("color-btn") && !clicktarget.classList.contains("active-border")){
+        clicktarget.classList.add("active-border");
+        activeButtoncolor.classList.remove("active-border");
+    }
+    if (clickTarget.classList.contains("btn") && !clickTarget.ClassList.cantains("active")){
+        clickTarget.classList.add("active");
+        activeButton.classList.remove("active");
+    }
+})
